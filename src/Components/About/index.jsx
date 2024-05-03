d95a16af48ab2939441806a67133b228836deac4
glcn375 <gulcin_375@hotmail.com>
2024-05-03 13:20:16 +0300

    fixed about section margin

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -6,7 +6,7 @@ export const About = () => {
         <section class="about-me" id="about">
             <h2 class="section__title section__title--about">Who I am</h2>
             <p class="section__subtitle section__subtitle--about">Front-End Developer based in Sweden</p>
-            <img src={myImg2} alt="User Image" class="about-me__img" />
+            <img src={myImg2} alt="User" class="about-me__img" />
             <div class="about-me__body">
                 <p>During my studies in Frontend Development program at Code2 Career, I gained hands-on experience in HTML, CSS, JavaScript, TypeScript and React which are essential skills for a frontend developer. I completed several projects that involved building responsive and user-friendly websites, showcasing my ability to translate design concepts into functional code.</p>
                 <p>Transitioned from education area to web development as a former teacher, I bring exceptional problem-solving skills and a strong grasp of educational tools and collaborative project management.</p>
