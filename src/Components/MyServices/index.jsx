38145b494f8c0438530f1bb4fdee2b27139b7b07
gulcin <gulcin_375@hotmail.com>
2025-07-12 00:03:57 +0300

    chore: updated service texts

--- src/Components/MyServices/index.jsx
+++ src/Components/MyServices/index.jsx
@@ -8,28 +8,29 @@ export const MyServices = () => {
         <div className="service">
           <h3>Web Development</h3>
           <p>
-            With a solid background in front-end development in React, I can
+            With a solid background in front-end development in React/TypeScript/NextJS, I can
             create responsive websites with high-quality code applying UI/UX
             aspects to make them functional, accessible and aesthetically
             engaging for users.
           </p>
         </div>
         <div className="service">
-          <h3>State Management</h3>
+          <h3>UI/UX Design</h3>
           <p>
-            Using Redux or Context API which provides maintaining a predictable
-            state container for JavaScript apps, I can create centralized state
-            management solutions throughout the project and increase the
-            performance of the application.
+            I specialize in crafting intuitive and visually engaging user
+            interfaces using Figma, especially for personal and portfolio
+            projects. I enjoy designing clean, simple interfaces and then
+            bringing them to life with front-end code.
           </p>
         </div>
         <div className="service">
-          <h3>External Libraries</h3>
+          <h3>External Libraries/State Management Tools</h3>
           <p>
-            Using third party libraries like MUI, TailwindCSS or React Hook
-            Form, I can improve code performance. I take advantage of the
-            built-in performance optimization features of the library or
-            framework, customizing them if necessary.
+            Using third party libraries like MUI, TailwindCSS or Daisy UI, I can
+            improve code performance. Using Redux or Context API which provides
+            maintaining a predictable state container for JavaScript apps, I can
+            create centralized state management solutions throughout the project
+            and increase the performance of the application.
           </p>
         </div>
       </div>
