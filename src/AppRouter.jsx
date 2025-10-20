8d4f933b7324790fb953ff0805f5ef485987f38c
gulcin <gulcin_375@hotmail.com>
2025-10-20 10:45:10 +0200

    fix: update imports to solve deployment error

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { MyServices } from "./components/MyServices";
-import { About } from "./components/About";
-import { MyWork } from "./components/MyWork";
-import { MyWorkDetail } from "./components/MyWorkDetail";
-import { Layout } from "./components/Layout";
-import { TechStack } from "./components/TechStack";
-import { NoteworthyProjects } from "./components/NoteworthyProjects";
+import { MyServices } from "./components/MyServices/index.jsx";
+import { About } from "./components/About/index.jsx";
+import { MyWork } from "./components/MyWork/index.jsx";
+import { MyWorkDetail } from "./components/MyWorkDetail/index.jsx";
+import { Layout } from "./components/Layout/index.jsx";
+import { TechStack } from "./components/TechStack/index.jsx";
+import { NoteworthyProjects } from "./components/NoteworthyProjects/index.jsx";
 import { Intro } from "./components/Intro/index.jsx"
 
 export const AppRouter = () => {
