0277d8a2c8af69847bb8e1a4174664427275e3ba
gulcin <gulcin_375@hotmail.com>
2025-11-12 23:06:37 +0100

    fix: add noreferrer to link

--- src/Components/NoteworthyProjects/index.jsx
+++ src/Components/NoteworthyProjects/index.jsx
@@ -12,7 +12,7 @@ export const NoteworthyProjects = () => {
         <section className={`noteworthy-projects ${i % 2 === 1 ? "reverse" : ""}`} id={project.id}>
           <h2 className="section__title section__title--noteworthy-projects">
             <span>{project.title}</span>
-            <a href={project.sourceCode} target="_blank">
+            <a href={project.sourceCode} target="_blank" rel="noreferrer">
               <FaGithub className="icon__github" />
             </a>
           </h2>
